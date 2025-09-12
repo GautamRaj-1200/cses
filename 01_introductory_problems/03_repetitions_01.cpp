9acc3ded2b8f3df5b7609300a2d5c61570108994
GautamRaj-1200 <gautamrajv2@gmail.com>
2025-09-12 06:35:28 +0530

    cses - repetitions

---
+++ 01_introductory_problems/03_repetitions_01.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void repetitions(string dna_sequence)
+{
+    int max_count = 1;
+    int count = 1;
+
+    for (int i = 1; i < dna_sequence.size(); i++)
+    {
+        if (dna_sequence[i] == dna_sequence[i - 1])
+        {
+            count++;
+        }
+        else
+        {
+            count = 1;
+        }
+        max_count = max(max_count, count);
+    }
+
+    cout << max_count;
+}
+
+int main()
+{
+    string dna_sequence;
+    cin >> dna_sequence;
+    repetitions(dna_sequence);
+    return 0;
+}
